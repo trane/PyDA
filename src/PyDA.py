7e223d93541e1e9d208393dd9cc24e049cc566c3
Landon GB <landogbland@gmail.com>
2011-11-29 10:06:46 -0700

    Add some simple method headers

--- src/PyDA.py
+++ src/PyDA.py
@@ -1,5 +1,8 @@
 # PyDA
 
+# List containing all of the PDA's that we will step through in the step method
+stepping = list();
+
 # A way to provide a pda as a string to this program
 
 # A structure to hold the pda
@@ -10,8 +13,10 @@
     # Have a function that takes a string being checked against the pda and moves
     # it forwards by one step
 
-# A way to step through the pda, one thing at a time, and display that input somehow
-    # SDL?
+# Steps through all of the PDA's in the stepping list by one move, creating new
+# PDA's if non-determinism kicks in, and adds those to the stepping list as well
+def step():
+    return
 
 # A way to non-deterministacally create more pdas as we step through them
     # Copy everything to a new pda data structure, then have the step method
