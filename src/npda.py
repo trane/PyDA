b51b5f4fbb3e8d0002478cf390acd06f73a31c28
Landon GB <landogbland@gmail.com>
2011-12-08 15:58:03 -0700

    Add functions to see if a string has been accepted or not in the pda

--- src/npda.py
+++ src/npda.py
@@ -1,6 +1,9 @@
 # This is the NDPA object file, dig it.
 
-# Steps though every stepper that we have by 1 step
+# Steps though every stepper that we have by 1 step.
+# Returns true if there are more states that can be stepped through, and
+# false if we cannot do any more stepping (ie if the string is not accepted
+# by the pda)
 def step_all(npda):
     # The new list of valid states after we step every stepper by 1
     new_valid_states = list()
@@ -12,9 +15,24 @@ def step_all(npda):
 
     # Set the new valid states to be the npda.stepper_list
     npda.stepper_list = list(new_valid_states)
-    return
+
+    # Return true if we can step again after this call, false otherwise
+    if not npda.stepper_list:
+        return False
+    return True
+
+
+# Returns true if this string has been accepted (ie, if the string has been
+# fully run through the pda and ends on a final state)
+def string_accepts(npda):
+    for s in npda.stepper_list:
+        if s.input == "":
+            if s.curr_state in npda.pda[f]:
+                return True
+    return False
+
 
 # Steps through this stepper by 1 step, and returns a list of all valid states
 # after this step
-def step_stepper():
+def step_stepper(s):
     return;
