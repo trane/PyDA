4fff30d25765cbfa64f4709a74e2204d29c4b1c4
Andrew Kuhnhausen <trane@xmission.com>
2011-12-08 16:37:34 -0700

    Move json load from file to util

--- src/PyDA_utils.py
+++ src/PyDA_utils.py
@@ -1,3 +1,20 @@
+import json
+
+def load_dfa(filename):
+    """
+    Loads a dfa from a given json formatted file
+    If the file loads and parses correctly, the pyda json structure is returned.
+    If there was an error loading or parsing the file, None is returned.
+    """
+    try:
+        json_data = open(filename)
+        pyda_struct = json.load(json_data)
+        json_data.close()
+    except Exception:
+        return None
+
+    return pyda_struct
+
 def normalize(pda):
     """ While JSON datastructure allows us portability, we will convert this
     into a more functional construct.
