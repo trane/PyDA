904c6f7286dac3725e3f497f5ab55a781d5c6baf
Colton Myers <colton.myers@gmail.com>
2011-12-09 10:21:05 -0700

    Move dotpda.py methods into PyDA_utils.py

--- src/PyDA_utils.py
+++ src/PyDA_utils.py
@@ -1,3 +1,19 @@
+"""
+PyDA_utils.py
+
+This file includes all of our utility methods, such as the JSON normalize
+method, and functionality to print NPDA objects to .dot and .pdf files
+
+Public API:
+    normalize(pda) - Convert JSON object to our dictionary PDA representation
+    pda2dot(npda_obj, pdaname) - Print npda_obj to 'pdaname.dot'
+    pda2pdf(npda_obj, pdaname) - Print npda_obj to 'pdaname.pdf'
+"""
+
+import os
+
+## Main external API methods ##################################################
+
 def normalize(pda):
     """ While JSON datastructure allows us portability, we will convert this
     into a more functional construct.
@@ -34,3 +50,104 @@ def normalize(pda):
     npda['F'] = set(pda['F'])
     npda['q0'] = pda['q0']
     return npda
+
+def pda2dot(npda_obj, pdaname):
+    """Generate a .dot file for the given NPDA object and pda_name
+
+    NOTE:  The .dot part of the filename should not be included in pdaname
+    """
+    with open(pdaname + '.dot', 'w') as fl:
+        prDotHeader(fl)
+        prNodeDefs(fl, npda_obj.pda)
+        prOrientation(fl)
+        prEdges(fl, npda_obj.pda)
+        prClosing(fl)
+
+def pda2pdf(npda_obj, pdaname):
+    """Generate a .pdf file for the given NPDA object and pda_name
+
+    NOTE:  The .pdf part of the filename should not be included in pdaname
+    """
+    with open(pdaname + '.dot', 'w') as fl:
+        prDotHeader(fl)
+        prNodeDefs(fl, npda_obj.pda)
+        prOrientation(fl)
+        prEdges(fl, npda_obj.pda)
+        prClosing(fl)
+
+    os.system("dot -Tps " + pdaname + ".dot > " + pdaname + ".ps")
+    os.system("ps2pdf13 " + pdaname + ".ps")
+    os.system("rm " + pdaname + ".dot " + pdaname + ".ps")
+
+
+## Helper methods #############################################################
+
+def dotsan_map(x):
+    """A homomorphism. We need to sanitize set() also. Ugh!
+    """
+    if x in {  "{",   " ",   "'",   "}" }:
+        return ""
+    elif x == ",":
+        return "_"
+    elif x == "(":
+        return "\("
+    elif x == ")":
+        return "\)"
+    else:
+        return x
+
+def homos(S,f):
+    """String homomorphism wrt lambda f
+    """
+    return "".join(map(f,S))
+
+def dot_san_str(S):
+    """Make dot like strings which are in set of states notation.
+    """
+    if S=="set()":
+        return "EMPTY_SET"
+    else:
+        return homos(S, dotsan_map)
+
+def prDotHeader(fl):
+    print (r'digraph G {', file=fl)
+    print (r'/* Defaults */', file=fl)
+    print (r'  fontsize = 12;', file=fl)
+    print (r'  ratio = compress; ', file=fl)
+    print (r'  rankdir=LR; ', file=fl)
+    print (r'/* Bounding box */', file=fl)
+    print (r'   size = "4,4";', file=fl)
+
+def prNonFinalNodeName(fl, q):
+    print (dot_san_str(q), r'[shape=circle, peripheries=1];', file=fl)
+
+def prFinalNodeName(fl, q):
+    print(dot_san_str(q), file=fl, end='')  # No \n
+    print(r' [shape=circle, peripheries=2];', file=fl)
+
+def prOrientation(fl):
+    print(r'/* Orientation */', file=fl)
+    print(r'orientation = portrait;', file=fl)
+
+def prEdges(fl, npda):
+    print(r'/* The graph itself */', file=fl)
+    print(r'""  -> ', dot_san_str(npda["q0"]), ";", file=fl)
+    for QcQ in npda["Delta"]:
+        print(dot_san_str(QcQ[0]), r' -> ', dot_san_str(QcQ[3]),
+              r'[label="', dot_san_str(QcQ[1]) + ',',
+              dot_san_str(QcQ[2]) + ';', dot_san_str(QcQ[4]), r'"];', file=fl)
+
+def prClosing(fl):
+    print(r'/* Unix command: dot -Tps exdfa.dot >! exdfa.ps */', file=fl)
+    print(r"/* For further details, see the `dot' manual    */", file=fl)
+    print(r"}", file=fl)
+
+def prNodeDefs(fl, npda):
+    print(r'/* Node definitions */', file=fl)
+    print(r'  "" [shape=plaintext];', file=fl)  # Start state arrow is from "" to I
+    # All non-accepts are single circles
+    for q in npda["Q"] - npda["F"]:
+        prNonFinalNodeName(fl, q)
+    for q in npda["F"]:
+        prFinalNodeName(fl, q)
+
