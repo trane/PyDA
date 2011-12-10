96a7e173ff225de3f2a825994bdfc8098c890253
Landon GB <landogbland@gmail.com>
2011-12-10 16:41:05 -0700

    Fix stupid python errors

--- src/tests/test1-sample2-0011.py
+++ src/tests/test1-sample2-0011.py
@@ -7,20 +7,19 @@ from npda import *
 from PyDA_utils import *
 
 # The string we will be testing
-test_string = "0011"
+test_string = "00001111"
 
 # Load the pda from the json file into our program
 the_pda = normalize(load_pda('../samples/sample2-0n1n.pyda'))
 n = NPDA(the_pda, test_string)
 
 # Step this string through the pda
-while n.can_step:
+while n.can_step():
     n.step_all()
     if n.accepts():
         break
 
 if n.accepts():
-    print("The string " + test_string + " satisfies this pda.")
+    print("The string \"" + test_string + "\" satisfies this pda.")
 else:
-    print("The string " + test_string + " does not satisfies this pda.")
-
+    print("The string \"" + test_string + "\" does not satisfies this pda.")
