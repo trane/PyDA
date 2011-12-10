b40713168732e09ad9e232602d59975a64f9a14b
Andrew Kuhnhausen <trane@xmission.com>
2011-12-10 12:28:29 -0700

    More useful assert failure

--- src/npda.py
+++ src/npda.py
@@ -47,7 +47,7 @@ class NPDA(object):
         assert pda['F'] <= pda['Q'], "Final state set too large"
         assert pda['Z'] in pda['Gamma'], "Initial stack symbol, not in Gamma"
         for inpt in pda['Delta']:
-            assert inpt[1] in pda['Sigma']|set("@"), inpt[1]
+            assert inpt[1] in pda['Sigma']|set("@"), "Invalid input in Delta"
         assert self.domain(pda['Delta']) <= self.product(
             self.product(
                 pda['Sigma']|set("@"), pda['Q']), pda['Gamma']|set("@")), "Delta too large"
