ffb5a1ef0de65cb07089c7d4cef7a3cca2f674d9
Andrew Kuhnhausen <trane@xmission.com>
2011-12-10 11:59:18 -0700

    Update normalize to work with new json structure
    NPDA now handles multiple push per (p,a,A)

--- src/PyDA_utils.py
+++ src/PyDA_utils.py
@@ -59,11 +59,12 @@ def normalize(pda):
     npda['Sigma'] = set(pda['Sigma'])
     npda['Gamma'] = set(pda['Gamma'])
     delta = set()
-    for init,inputs in pda['Delta'].items():
-        for inpt,stack in inputs.items():
-            for stack_t,dest in stack.items():
-                for state in dest[0]:
-                    delta.add((init, inpt, stack_t, state, dest[1]))
+    for init,inputs in pda['Delta'].items():      # state: input
+        for inpt,stack in inputs.items():         # input: top of stack
+            for stack_t,dest in stack.items():    # top of stack: dest state
+                for state,pushes in dest.items(): # state: [push1, push2]
+                    for push in pushes:
+                        delta.add((init, inpt, stack_t, state, push))
     npda['Delta'] = delta
     npda['F'] = set(pda['F'])
     npda['q0'] = pda['q0']
