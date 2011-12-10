d7c904f61c8e292c37e9a0df6333d0ed0e55afac
Colton Myers <colton.myers@gmail.com>
2011-12-10 14:43:04 -0700

    Add tests/ and first test

---
+++ src/tests/test1-sample2-0011.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Test sample2 with the string "0011"
+
+from npda import *
+from PyDA_utils import *
+
+the_pda = normalize(load_pda('samples/sample2-0n1n.pyda'))
+n = NPDA(the_pda, "0011")
+
+for i in range(10):
+    n.step_all()
+    if n.accepts():
+        print("WOOHOO!")
+        break
