1e64fdf06fad0089ab931b9c3a574c34895733ef
Landon GB <landogbland@gmail.com>
2011-12-10 19:30:19 -0700

    Expand on the outline for the main method

--- src/PyDA_cli.py
+++ src/PyDA_cli.py
@@ -1,4 +1,6 @@
 import argparse
+from npda import *
+from PyDA_utils import *
 
 parser = argparse.ArgumentParser(description='Run an NPDA on a string.')
 parser.add_argument('string', metavar='"S"', type=str, nargs='+',
@@ -11,19 +13,33 @@ parser.print_help()
 def to_dot():
     print("")
 
-# main method
-#
-# Verify arguements using argparse
-#
-# if the step flag isn't set, just test if the pda accepts the string and return the results
-#
-# if the step flag is set, go into a loop:
-#
-#   Get intput from user:
-#   q = quit
-#   f ID = freeze the stepper with that id
-#   s = steps through everything that isn't frozen by 1 step
-#   p = print the current data from the steppers, distingusihg between frozen and active
-#
-#   Act according to the input. Need to decide how to close (think don't close the
-#   program until explicatally told to by the user pressing q)
+def main():
+    # Verify arguements using argparse
+    # pdf_file
+    # step
+    # test_string
+
+    # Create the npda
+    pda = normalize(load_pda(pda_file))
+    n = NPDA(the_pda, test_string)
+
+    # Default behaivor, check if the string satisfies the pda
+    if(step == False):
+        if check_acceptance(n) == True:
+            print("The string \"" + test_string + "\" satisfies this pda.")
+        else:
+            print("The string \"" + test_string + "\" does not satisfies this pda.")
+        return
+
+    # Main event loop for stepping through the program
+    while True:
+        #   Get intput from user:
+        #   q = quit
+        #   f ID = freeze the stepper with that id
+        #   s = steps through everything that isn't frozen by 1 step
+        #   p = print the current data from the steppers, distingusihg between frozen and active
+        #   pdf FILENAME = create a pdf document of the current pda
+        #   dot FILENAME = create a dotty document of the current pda
+        #
+        #   Act according to the input. Need to decide how to close (think don't close the
+        #   program until explicatally told to by the user pressing q)
